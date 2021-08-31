c4433bc2a65cf68691181db42388c9de517dde0b
Eddie Jung <ejung716@gmail.com>
2021-08-31 10:04:24 -0700

    Add slice effect in redrawActiveSlice function.

--- Project23/GameScene.swift
+++ Project23/GameScene.swift
@@ -108,6 +108,20 @@ class GameScene: SKScene {
             activeSliceFG.path = nil
             return
         }
+        
+        if activeSlicePoints.count > 12 {
+            activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+        
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
     }
     
 }
